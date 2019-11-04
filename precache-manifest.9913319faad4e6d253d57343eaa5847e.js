cfccd334fe93f5ad47510e69ee3bec87e228fc30
Weerasooriya <Pandula.Weerasooriya@lseg.com>
2019-11-04 15:37:57 +0530

    Updates

---
+++ precache-manifest.9913319faad4e6d253d57343eaa5847e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7daf43fa05083973b4bcc0f9ae19301",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e4511dd88e73c1238a0",
+    "url": "/monsters-rolodex/static/css/main.699252bb.chunk.css"
+  },
+  {
+    "revision": "331a6c9f26a1f158523b",
+    "url": "/monsters-rolodex/static/js/2.f641515a.chunk.js"
+  },
+  {
+    "revision": "5e4511dd88e73c1238a0",
+    "url": "/monsters-rolodex/static/js/main.ea892f15.chunk.js"
+  },
+  {
+    "revision": "cf4959d4a3aa9299c4ea",
+    "url": "/monsters-rolodex/static/js/runtime-main.92f2d94f.js"
+  }
+]);
